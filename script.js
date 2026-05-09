8bd28a68a397cb32d215709cbffb4a9a08ed4057
devops_harsha <pekalaharshavardhan.23.csm@anits.edu.in>
2026-05-09 11:10:00 +0530

    Developed landing page

---
+++ script.js
@@ -0,0 +1,5 @@
+const alertBtn = document.getElementById("alertBtn");
+
+alertBtn.addEventListener("click", () => {
+  alert("Congratulations! You learned how to push code from VS Code to GitHub.");
+});
